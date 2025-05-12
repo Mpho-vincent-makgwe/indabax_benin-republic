f459f44a313aa871deb800b5ef28dcde9cc98d7f
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2025-05-12 10:53:57 +0200

    Figuring things out

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "IndabaX Benin Republic" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
